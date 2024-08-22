5d4a0a134902c1c4b879fa2ebe6fb76b7a9a27c8
Nikhil S <129724432+nik1740@users.noreply.github.com>
2024-08-23 00:04:44 +0530

    Update grades.py

--- grades.py
+++ grades.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import pickle
 
 # Step 1: Load Data from a CSV File
 csv_file = 'synthetic_student_data.csv'
@@ -19,6 +20,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Step 4: Train the Model
 model = LinearRegression()
 model.fit(X_train, y_train)
+
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
 
 # Step 5: Make Predictions
 y_pred = model.predict(X_test)
